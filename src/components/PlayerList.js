8c13c2f82289b4138a0533a0cc3486e10f043283
Mark Chuang <markchuangth@gmail.com>
2022-01-16 18:37:03 -0500

    first commit

---
+++ src/components/PlayerList.js
@@ -0,0 +1,15 @@
+import React from 'react'; //optional
+
+function TweetForm(){
+  let text = "what are you humming about?";
+  
+  return (
+
+    <section className="PlayerList">
+    <h1>Current participating players</h1>
+    {/* Players will be shown here */}
+    </section>
+  );
+}
+
+export default TweetForm;
